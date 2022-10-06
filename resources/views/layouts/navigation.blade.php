fb964999edf805bb44c24aefaee969c92436e60b
willam-gm <will-gm@hotmail.com>
2022-10-05 23:21:54 -0400

    Cambiar de logo

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
+                        <img src="{{asset('/img/logo.svg')}}" alt="LOGO" width="60">
                     </a>
                 </div>
 
